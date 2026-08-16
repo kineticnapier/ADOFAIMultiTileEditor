0ce1d0b8b3409cd66e8a8bad13b6853fd6ebcb05
Kinetic Napier <u61819269@gmail.com>
2026-08-17 07:24:53 +0900

    Restore tile decoration result type

---
+++ src/TileDecorationResult.cs
@@ -0,0 +1,10 @@
+namespace KineticNapier.ADOFAIMultiTileEditor
+{
+    internal sealed class TileDecorationResult
+    {
+        internal int Created;
+        internal int Replaced;
+        internal int SkippedTracks;
+        internal string Diagnostic;
+    }
+}
