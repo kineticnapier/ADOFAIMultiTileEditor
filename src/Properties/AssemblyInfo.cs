b1cd3de7268766461e15ef57d61ad93c5651cdf3
Kinetic Napier <u61819269@gmail.com>
2026-08-24 11:32:19 +0900

    Align MTE assembly version 0.10.8

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("ADOFAIMultiTileEditor")]
 [assembly: ComVisible(false)]
 [assembly: Guid("779a8f14-2d3e-4d6c-9f3c-92a87d88e5af")]
-[assembly: AssemblyVersion("0.10.7.0")]
-[assembly: AssemblyFileVersion("0.10.7.0")]
+[assembly: AssemblyVersion("0.10.8.0")]
+[assembly: AssemblyFileVersion("0.10.8.0")]
