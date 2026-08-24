9904e5428a92708d0cbf92ac95b2a29ed9d26486
Kinetic Napier <u61819269@gmail.com>
2026-08-24 12:44:57 +0900

    Set MTE assembly version to 0.10.16

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("ADOFAIMultiTileEditor")]
 [assembly: ComVisible(false)]
 [assembly: Guid("779a8f14-2d3e-4d6c-9f3c-92a87d88e5af")]
-[assembly: AssemblyVersion("0.10.15.0")]
-[assembly: AssemblyFileVersion("0.10.15.0")]
+[assembly: AssemblyVersion("0.10.16.0")]
+[assembly: AssemblyFileVersion("0.10.16.0")]
